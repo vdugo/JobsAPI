1dde8581887d0ce55bbcba34c3e549ccf298097e
unknown <v3rdugo@yahoo.com>
2022-06-21 22:00:45 -0700

    test login user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors/')
+const { BadRequestError, UnauthenticatedError } = require('../errors/')
 
 const register = async (req, res) => {
     const user = await User.create({...req.body})
@@ -9,7 +9,22 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send('login user')
+    const { email, password } = req.body
+
+    if (!email || !password)
+    {
+        throw new BadRequestError('Please provide a valid email and password')
+    }
+
+    const user = await User.findOne({email})
+
+    if (!user)
+    {
+        throw new UnauthenticatedError('Invalid credentials')
+    }
+
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({user: {name: user.name}, token})
 }
 
 module.exports = {
