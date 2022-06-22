e706362c0c23fe6aa289c56cab6faa800b64f57b
unknown <v3rdugo@yahoo.com>
2022-06-21 19:27:39 -0700

    refactor code to hash the password in the User model

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,11 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors/')
+const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body
-    const user = await User.create({...req.body}) 
+    
+    const user = await User.create({...tempUser})
     res.status(StatusCodes.CREATED).json(user)
 }
 
